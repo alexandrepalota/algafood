469e647e17dd52b22c0147dbcd6c957f5e2df2e8
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-06-02 21:42:34 -0300

    Criando operações básicas de CRUD com JPA

---
+++ src/main/java/com/algaworks/algafoodapi/jpa/ExclusaoCozinha.java
@@ -0,0 +1,24 @@
+package com.algaworks.algafoodapi.jpa;
+
+import com.algaworks.algafoodapi.AlgafoodApiApplication;
+import com.algaworks.algafoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class ExclusaoCozinha {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setId(1L);
+
+        cadastroCozinha.remover(cozinha1);
+
+    }
+}
