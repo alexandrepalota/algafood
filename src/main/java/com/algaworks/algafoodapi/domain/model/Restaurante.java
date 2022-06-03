a2370456e99a16fd8ce8a28a2cd16aa2e78f94b4
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-06-03 09:03:46 -0300

    Utilizando a biblioteca Lombok

--- src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Restaurante.java
@@ -1,12 +1,18 @@
 package com.algaworks.algafoodapi.domain.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data // contém Getters, Setters, AllArgsConstructor, ToString, Equals e HashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_restaurante")
 public class Restaurante {
 
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,52 +23,4 @@ public class Restaurante {
     @Column(name = "vl_frete")
     private BigDecimal taxaFrete;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public BigDecimal getTaxaFrete() {
-        return taxaFrete;
-    }
-
-    public void setTaxaFrete(BigDecimal taxaFrete) {
-        this.taxaFrete = taxaFrete;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Restaurante other = (Restaurante) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
 }
