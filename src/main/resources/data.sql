1c847d3fa438e6b790f148409d1a8db5d026a26e
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-06-03 09:08:49 -0300

    Repository para Restaurante

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,7 @@
 insert into tb_cozinha (nm_cozinha) values ('Tailandesa');
 insert into tb_cozinha (nm_cozinha) values ('Indiana');
+
+insert into tb_restaurante (nm_restaurante, vl_frete) values ('India Food', '12.00');
+insert into tb_restaurante (nm_restaurante, vl_frete) values ('Palácio Tailandês', '8.00');
+insert into tb_restaurante (nm_restaurante, vl_frete) values ('Prato Exótico', '9.00');
+insert into tb_restaurante (nm_restaurante, vl_frete) values ('Namastê', '0.00');
