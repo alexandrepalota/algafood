84b1d7998eda3a57eb089e6b144f0f997de3492c
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-06-26 15:13:21 -0300

    Customizando a representação em XML com Wrapper e anotações do Jackson

--- src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafoodapi/api/controller/CozinhaController.java
@@ -1,8 +1,10 @@
 package com.algaworks.algafoodapi.api.controller;
 
+import com.algaworks.algafoodapi.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafoodapi.domain.model.Cozinha;
 import com.algaworks.algafoodapi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,11 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhasXmlWrapper listarXml() {
+        return new CozinhasXmlWrapper(cozinhaRepository.listar());
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
@@ -8,20 +8,18 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
-@JsonRootName("gastronomia") // aplicável ao xml
+@JsonRootName("cozinha") // aplicável ao xml
 @Data // contém Getters, Setters, AllArgsConstructor, ToString, Equals e HashCode
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "tb_cozinha")
 public class Cozinha {
 
-    @JsonIgnore
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonProperty("titulo")
     @Column(name = "nm_cozinha", nullable = false)
     private String nome;
 
