7e1b574937c2226460bac5d16b2378651e0f1d3b
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-07-02 07:16:51 -0300

    Externalizando query para arquivo xml

--- src/main/java/com/algaworks/algafoodapi/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafoodapi/domain/repository/RestauranteRepository.java
@@ -15,7 +15,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
     List<Restaurante> findAllByNomeContainingIgnoreCaseAndCozinhaId(String nome, Long cozinhaId);
 
-    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+//    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
     List<Restaurante> consultarPorNomeECozinha(String nome, @Param("id") Long cozinhaId);
 
     // Prefixos de QueryMethods
