ef64b744885402f8aa9eaa5f9235b208bb526d52
Alexandre Palota da Silva <alexandre.palota@gmail.com>
2022-07-08 17:42:05 -0300

    Flyway - criando e evoluindo o banco de dados com novas migrações

---
+++ src/main/resources/db/migration/V001__criacao-inicial.sql
@@ -0,0 +1,5 @@
+create table tb_cozinha (
+	id serial not null,
+	nome varchar(60) not null,
+	primary key (id)
+);
---
+++ src/main/resources/db/migration/V002__cria-tabela-cidade.sql
@@ -0,0 +1,6 @@
+create table tb_cidade (
+	id serial not null,
+	nome_cidade varchar(80) not null,
+	nome_estado varchar(80) not null,
+	primary key (id)
+);
